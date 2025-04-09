486dc8c4cdf44849a11ececca87a8af26dfdcf20
Jake Sorce <jake.sorce@tavahealth.com>
2025-04-09 16:02:05 -0600

    Vite starter app with readme

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import tavaLogo from "./assets/tava-logo.svg";
+
+function App() {
+  return (
+    <a href="https://tavahealth.com" target="_blank">
+      <img src={tavaLogo} className="logo" alt="Tava Health Logo" />
+    </a>
+  );
+}
+
+export default App;
